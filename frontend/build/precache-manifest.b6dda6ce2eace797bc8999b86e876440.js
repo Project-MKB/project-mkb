a8ff663eaa4ac96752293ccce1238c0ad2ac06f1
Minjun Youn <sdymj84@gmail.com>
2019-10-23 11:55:56 -0500

    Update foldering for separate hosting (#1)
    * Add initial DB/API
    
    * Update to separate folder
    
    * test server
    
    * test
    
    * Update foldering for separate hosting

---
+++ frontend/build/precache-manifest.b6dda6ce2eace797bc8999b86e876440.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdeafec196707effe612de340400833c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce74bf11e94de6ef9c1f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fca8b03a30a63dfc8031",
+    "url": "/static/js/2.5b9051c8.chunk.js"
+  },
+  {
+    "revision": "ce74bf11e94de6ef9c1f",
+    "url": "/static/js/main.466c1349.chunk.js"
+  },
+  {
+    "revision": "e10d001fbb37321e37f1",
+    "url": "/static/js/runtime-main.c92a913f.js"
+  }
+]);
