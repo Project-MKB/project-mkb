ebdd5f5cd3a7d6465a27ab04355e3bca1e9850f3
Minjun Youn <minjun1.y@partner.samsung.com>
2019-10-24 16:46:38 -0500

    save

--- backend/tests/sample.test.js
+++ backend/tests/sample.test.js
@@ -1,3 +1,34 @@
+const firebase = require('../fbConfig')
+const firebasemock = require('firebase-mock')
+const mockauth = new firebasemock.MockAuthentication();
+
+
+
+const users = {
+  create: async function (credentials) {
+    try {
+      return await firebase.auth().createUserWithEmailAndPassword(credentials);
+    } catch (error) {
+      console.error(error)
+    }
+
+  }
+};
+
+
+
+
+users.create({
+  email: 'ben@example.com',
+  password: 'examplePass'
+});
+// mockauth.auth().flush();
+
+// mockauth.auth().getUserByEmail('ben@example.com').then(function (user) {
+//   console.assert(user, 'ben was created');
+// });
+
+
 describe('Sample test', () => {
   it('should run jest', () => {
     expect(true).toBe(true)
