015442733e5187d544e931c057eba14253e0c717
Minjun Youn <sdymj84@gmail.com>
2019-10-31 22:39:53 -0500

    Add Sign up API / Test / Doc (#12)
    * Update to separate folder
    
    * test server
    
    * test
    
    * Update foldering for separate hosting
    
    * Update foldering for separate hosting (#1)
    
    * Add initial DB/API
    
    * Update to separate folder
    
    * test server
    
    * test
    
    * Update foldering for separate hosting
    
    * Add dev/start script on package.json
    
    * Add signup api - in progress
    
    * Update signup api and doc
    
    * save
    
    * Add test for signup
    
    * Add signin api / test
    
    * working on user info update api
    
    * Add update userinfo api and refactor code

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+const recipesRouter = require('./routes/recipes')
+const usersRouter = require('./routes/users')
+
+app.use('/recipes', recipesRouter)
+app.use('/users', usersRouter)
+
+
+
+module.exports = app
---
+++ backend/util/validators.js
@@ -0,0 +1,49 @@
+const validator = require('validator')
+
+exports.validateSignupData = newUser => {
+  const error = {}
+  if (validator.isEmpty(newUser.email)) {
+    error.code = "auth/empty-email"
+    error.message = "Email must not be empty."
+  } else if (!validator.isEmail(newUser.email)) {
+    error.code = "auth/invalid-email"
+    error.message = "The email address is badly formatted."
+  }
+  if (validator.isEmpty(newUser.password)) {
+    error.code = "auth/empty-password"
+    error.message = "Password must not be empty."
+  } else if (validator.isEmpty(newUser.confirmPassword)) {
+    error.code = "auth/empty-confirmPassword"
+    error.message = "Confirm Password must not be empty"
+  } else if (newUser.password !== newUser.confirmPassword) {
+    error.code = "auth/password-not-match"
+    error.message = "Password must match"
+  }
+
+  return {
+    error,
+    valid: Object.keys(error).length === 0 ? true : false
+  }
+}
+
+
+
+exports.validateSigninData = user => {
+  const error = {}
+  if (validator.isEmpty(user.email)) {
+    error.code = "auth/empty-email"
+    error.message = "Email must not be empty."
+  } else if (!validator.isEmail(user.email)) {
+    error.code = "auth/invalid-email"
+    error.message = "The email address is badly formatted."
+  }
+  if (validator.isEmpty(user.password)) {
+    error.code = "auth/empty-password"
+    error.message = "Password must not be empty."
+  }
+
+  return {
+    error,
+    valid: Object.keys(error).length === 0 ? true : false
+  }
+}
