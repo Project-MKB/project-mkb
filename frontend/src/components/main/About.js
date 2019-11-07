5dc05c3fc24239ab26a483d76465cf96059dfbac
Minjun Youn <sdymj84@gmail.com>
2019-11-07 14:13:11 -0600

    Redux skeleton (#36)
    * Update to separate folder
    
    * test server
    
    * test
    
    * Update foldering for separate hosting
    
    * Update foldering for separate hosting (#1)
    
    * Add initial DB/API
    
    * Update to separate folder
    
    * test server
    
    * test
    
    * Update foldering for separate hosting
    
    * Add dev/start script on package.json
    
    * Add signup api - in progress
    
    * Update signup api and doc
    
    * save
    
    * Add test for signup
    
    * Add signin api / test
    
    * working on user info update api
    
    * Add update userinfo api and refactor code
    
    * Update backend readme for schedule
    
    * Add redux files
    
    * Add redux files
    
    * adding redux for login
    
    * Update login redux cycle / Cleanup code using prettier
    
    * Update minor UI on login
    
    * Add register redux and signout comp
    
    * adding getUser logic when loading app
    
    * adding getUser logic when loading app
    
    * Update App.js to check auth state on loading
    
    * Update readme
    
    * Update readme
    
    * Update readme
    
    * Update login page with error message section
    
    * Update userActions.js to print signout message to console

--- frontend/src/components/main/About.js
+++ frontend/src/components/main/About.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return(
-        <div className="container-fluid" id="about">
-            <div className="row">
-                <div className="col-12 col-md-6">
-                    <h3>
-                        About Project MKB
-                    </h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam quaerat maiores recusandae excepturi, eius temporibus totam odit obcaecati autem impedit eos dolorum reiciendis eum consectetur, fuga sed facilis in exercitationem.</p>
-                </div>
-                <div className="col-12 col-md-6">
-                    <img src="https://place-hold.it/300" alt="" className="img-fluid"/>
-                </div>
-            </div>
+  return (
+    <div className="container-fluid" id="about">
+      <div className="row">
+        <div className="col-12 col-md-6">
+          <h3>About Project MKB</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            quaerat maiores recusandae excepturi, eius temporibus totam odit
+            obcaecati autem impedit eos dolorum reiciendis eum consectetur, fuga
+            sed facilis in exercitationem.
+          </p>
         </div>
-    );
-}
+        <div className="col-12 col-md-6">
+          <img src="https://place-hold.it/300" alt="" className="img-fluid" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Main = () => <h1>This is the Main page</h1>;
 
