06ac877c699e497c0653e674aa3ae30bb1f654ec
kststudio <kststudio@gmail.com>
2019-11-06 22:32:28 -0500

    finishes up state

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import Nav from '../components/global/Footer';
 
 class Login extends Component {
-    constructor(props) {
-        super(props)
+    constructor() {
+        super();
         this.state = {
             email: '',
             password: ''
         }
     }
 
-    this.setState = {
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
 
-    }
+        console.log(event.target.value);
+      }
+
+      handleSubmit(event) {
+        console.log(this.state.value);
+        event.preventDefault();
+      }
+    
     render(){
         return(
             <div id="container">
@@ -23,15 +37,15 @@ class Login extends Component {
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-12 col-md-4">
-                        <form>
+                        <form onSubmit={this.handleSubmit}>
                             <div className="form-group">
                                 <label htmlFor="exampleInputEmail1">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email" onChange={this.handleInputChange.bind(this)} value={this.state.email} />
                                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="exampleInputPassword1">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" name="password" onChange={this.handleInputChange.bind(this)} value={this.state.password} />
                             </div>
                             <div className="form-group form-check">
                                 <input type="checkbox" className="form-check-input" id="exampleCheck1" />
