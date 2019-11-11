eaa4fee2c2cf82e5d3c8c014cab6f240cd93398d
Minjun Youn <minjun1.y@partner.samsung.com>
2019-11-11 16:52:07 -0600

    Fixes #29
    1. Update user update page
    2. Connect complete redux cycle
    3. Add Badge.js to show and edit user preferences
    4. Update backend/README.md - edit request body for /users/update api

--- frontend/src/pages/updateUser.js
+++ frontend/src/pages/updateUser.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { updateUser } from "../redux/actions/userActions";
-import checkWhyRerender from "../util/checkWhyRerender";
+import "./updateUser.scss";
+import Badge from "../components/global/Badge";
+import LoadingButton from "../components/global/LoadingButton";
 
 export class UpdateUser extends Component {
   state = {
     displayName: "",
+    preference: "", // added this for input field
     preferences: [],
-    photoURL: "",
     country: "",
     cuisine: ""
   };
@@ -16,7 +18,6 @@ export class UpdateUser extends Component {
     this.setState({
       displayName: user.displayName || "",
       preferences: user.preferences || [],
-      photoURL: user.photoURL || "",
       country: user.country || "",
       cuisine: user.cuisine || ""
     });
@@ -26,7 +27,7 @@ export class UpdateUser extends Component {
     this.updateStateFromProps(this.props.user);
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps) {
     if (this.props.user.isLoading !== prevProps.user.isLoading) {
       this.updateStateFromProps(this.props.user);
     }
@@ -38,33 +39,112 @@ export class UpdateUser extends Component {
     });
   };
 
+  // Add badge when pressing add button
+  handlePrefAdd = () => {
+    const newPref = this.state.preference.trim().toLowerCase();
+    if (newPref) {
+      this.setState(prevState => {
+        if (prevState.preferences.indexOf(newPref) > -1) {
+          alert("Tag already exists");
+          return prevState;
+        } else {
+          return {
+            preferences: [...prevState.preferences, newPref]
+          };
+        }
+      });
+    }
+  };
+
+  // Remove badge when pressing x button on the badge
+  handlePrefRemove = prefToRemove => {
+    this.setState(prevState => {
+      return {
+        preferences: prevState.preferences.filter(pref => pref !== prefToRemove)
+      };
+    });
+  };
+
   handleSubmit = e => {
     e.preventDefault();
     // Call action to update user info
+    this.props.updateUser(this.state, this.props.history);
   };
 
   render() {
     const { user } = this.props;
-    if (user.isLoading) {
-      return <div>Loading...</div>;
-    }
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="displayName">Display Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="displayName"
-            onChange={this.handleChange}
-            value={this.state.displayName}
-          />
+      <div className="mt-5">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-4">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="displayName">Display Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="displayName"
+                  onChange={this.handleChange}
+                  value={this.state.displayName}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="preference">Preference</label>
+                <div>
+                  <input
+                    type="text"
+                    className="form-control pref"
+                    id="preference"
+                    onChange={this.handleChange}
+                    value={this.state.preference}
+                  />
+                  <button
+                    type="button"
+                    onClick={this.handlePrefAdd}
+                    className="btn btn-outline-success"
+                  >
+                    ADD
+                  </button>
+                </div>
+              </div>
+
+              {this.state.preferences.map((pref, i) => (
+                <Badge
+                  key={i}
+                  pref={pref}
+                  handlePrefRemove={this.handlePrefRemove}
+                />
+              ))}
+
+              <div className="form-group">
+                <label htmlFor="country">Country</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="country"
+                  onChange={this.handleChange}
+                  value={this.state.country}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="cuisine">Cuisine</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="cuisine"
+                  onChange={this.handleChange}
+                  value={this.state.cuisine}
+                />
+              </div>
+
+              <LoadingButton isLoading={user.isLoading} />
+            </form>
+          </div>
         </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      </div>
     );
   }
 }
--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -91,8 +91,33 @@ export const signoutUser = () => dispatch => {
 };
 
 // update user info
-export const updateUser = user => dispatch => {
+export const updateUser = (userInfo, history) => async dispatch => {
   console.log("update user action");
+  dispatch({ type: "USER_UPDATE_REQUEST" });
+
+  try {
+    // call update api
+    const res = await axios.post(
+      "http://localhost:5000/users/update",
+      userInfo
+    );
+
+    dispatch({
+      type: "USER_UPDATE_SUCCESS",
+      payload: res.data
+    });
+
+    alert("User info is successfully updated!");
+    // redirect to home page when login process finishes
+    history.push("/");
+  } catch (e) {
+    const error = e.response.data.error;
+    console.log(error);
+    dispatch({
+      type: "USER_UPDATE_FAILURE",
+      payload: error
+    });
+  }
 };
 
 // save token to local storage to use it later
