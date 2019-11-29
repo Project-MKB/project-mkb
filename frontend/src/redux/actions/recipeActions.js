28ed701203ff86c618a64c067270666829c64344
Minjun Youn <sdymj84@gmail.com>
2019-11-29 16:44:08 -0600

    Update recipe - fixes #31 (#44)
    * Initial commit
    
    * Add initial DB/API
    
    * Update to separate folder
    
    * test server
    
    * test server
    
    * test
    
    * test
    
    * Update foldering for separate hosting
    
    * Update foldering for separate hosting (#1)
    
    * Add initial DB/API
    
    * Update to separate folder
    
    * test server
    
    * test
    
    * Update foldering for separate hosting
    
    * Add dev/start script on package.json
    
    * Add signup api - in progress
    
    * Update signup api and doc
    
    * save
    
    * Add test for signup
    
    * Add signin api / test
    
    * working on user info update api
    
    * Add update userinfo api and refactor code
    
    * Update backend readme for schedule
    
    * Update readme
    
    * Update readme
    
    * Update readme
    
    * Add createRecipe.js and redux action/reducers for creating recipe
    
    * Add heroku deploy instruction to backend/README.md
    
    * Add Procfile for Heroku deployment
    
    * Update Procfile and package.json for server start script
    
    * Update package.json to update server start script
    
    * .
    
    * .
    
    * .
    
    * Update backend/readme.md to update heroku deploy instruction
    
    * Update fbAdminConfig.js to refer secret keys from .env
    
    * Update fbAdminConfig.js to refer secret keys from .env
    
    * Update route to auth route for main/details
    
    * Delete unused files
    
    * Delete unused files
    
    * .
    
    * .
    
    * Delete recipes.js
    
    * Delete users.js
    
    * Delete recipes.js
    
    * Delete users.js
    
    * delete unused files
    
    * .
    
    * Update details.js to correctly import Hero / Update RecipeInstructions.js to change class to className
    
    * Add update recipe page and redux
    
    * Update update recipe api to inclue clone
    
    When user trys to update the recipe that they created, it'll just update it
    When user trys to update other person's recipe, it'll clone first and update the cloned recipe
    
    * Update detail page - add button to redirect to update recipe page. fixes #46

--- frontend/src/redux/actions/recipeActions.js
+++ frontend/src/redux/actions/recipeActions.js
@@ -1,5 +1,27 @@
 import axios from "axios";
 
+// get recipe action
+export const getRecipe = id => async dispatch => {
+  dispatch({ type: "RECIPE_GET_REQUEST" });
+
+  try {
+    // call get recipe api
+    const res = await axios.get(`http://localhost:5000/recipes/get/${id}`);
+
+    dispatch({
+      type: "RECIPE_GET_SUCCESS",
+      payload: res.data
+    });
+  } catch (e) {
+    const error = e.response.data.error;
+    console.log(error);
+    dispatch({
+      type: "RECIPE_GET_FAILURE",
+      payload: error
+    });
+  }
+};
+
 // create recipe action
 export const createRecipe = (newRecipe, history) => async dispatch => {
   dispatch({ type: "RECIPE_CREATE_REQUEST" });
@@ -23,6 +45,34 @@ export const createRecipe = (newRecipe, history) => async dispatch => {
     console.log(error);
     dispatch({
       type: "RECIPE_CREATE_FAILURE",
+      payload: error
+    });
+  }
+};
+
+// update recipe action
+export const updateRecipe = (id, recipe, history) => async dispatch => {
+  dispatch({ type: "RECIPE_UPDATE_REQUEST" });
+
+  try {
+    // call update recipe api
+    const res = await axios.post(
+      `http://localhost:5000/recipes/update/${id}`,
+      recipe
+    );
+
+    dispatch({
+      type: "RECIPE_UPDATE_SUCCESS",
+      payload: res.data
+    });
+
+    // redirect to details page when update process finishes
+    history.push("/details");
+  } catch (e) {
+    const error = e.response.data.error;
+    console.log(error);
+    dispatch({
+      type: "RECIPE_UPDATE_FAILURE",
       payload: error
     });
   }
